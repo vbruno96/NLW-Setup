0de1291f68e11e402dedb98df26c76bbd4064195
Bruno Vinícius <vbruno96@gmail.com>
2023-01-18 23:33:49 -0300

    create modal and popover

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,14 @@
+import * as Progress from '@radix-ui/react-progress';
+
+interface ProgressProps {
+  progress: number
+}
+
+export function ProgressBar({ progress }: ProgressProps) { 
+  
+  return (
+    <Progress.Root value={progress} className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <Progress.Indicator className="h-3 rounded-xl bg-violet-600" style={{width: `${progress}%`}} />
+    </Progress.Root>
+  )
+}
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -29,7 +29,7 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => (
-          <HabitDay key={date.toString()}/>
+          <HabitDay amount={5} completed={4} key={date.toString()}/>
         ))}
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
           <div key={i} className="w-10 h-10 bg-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
